86a0eb188f9a51f2fa2099f0c5660d8a2579799b
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2023-10-08 00:32:42 +0530

    v1

--- Etherium/Contract/Compaign.sol
+++ Etherium/Contract/Compaign.sol
@@ -1,76 +1,79 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
-contract FactoryCompaign{
+contract FactoryCompaign {
     address[] public deployedCompaigns;
 
-    constructor(uint minimum){
-    address newCompaign=new Compaign(minimum,msg.sender);
-    deployedCompaigns.push(newCompaign);
+    constructor(uint minimum) {
+        address newCompaign = address(new Compaign(minimum, msg.sender));
+        deployedCompaigns.push(newCompaign);
     }
 
-    function getDeployedCompaigns() public view returns(address[] memory){
+    function getDeployedCompaigns() public view returns (address[] memory) {
         return deployedCompaigns;
     }
 }
 
-contract Compaign{
-    struct Request{
+contract Compaign {
+    struct Request {
         string description;
         uint value;
         address payable vendor;
         bool complete;
         uint yesVoterCount;
-        mapping(address=>bool) yesVoters;
+        mapping(address => bool) yesVoters;
     }
 
     address public manager;
     uint public minimumContribution;
-    mapping(address=>bool) public isContributers;
+    mapping(address => bool) public isContributers;
     uint public isContributersCount;
 
     uint numRequests;
-    mapping(uint=>Request) public requests;
+    mapping(uint => Request) public requests;
 
     modifier restricted() {
-        require(msg.sender==manager);
+        require(msg.sender == manager);
         _;
     }
 
-
     constructor(uint minimum, address creator) {
-        manager=creator;
-        minimumContribution=minimum;
+        manager = creator;
+        minimumContribution = minimum;
     }
 
-function contribute() public payable {
-    require(msg.value>minimumContribution);
-    isContributers[msg.sender]=true;
-    isContributersCount++;
-}
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+        isContributers[msg.sender] = true;
+        isContributersCount++;
+    }
 
-function createRequest(string memory description,uint value,address payable vendor) public restricted {
-    Request storage newRequest=requests[numRequests++];
-        newRequest.description=description;
-        newRequest.value=value;
-        newRequest.vendor=vendor;
-        newRequest.complete=false;
-        newRequest.yesVoterCount=0;
+    function createRequest(
+        string memory description,
+        uint value,
+        address payable vendor
+    ) public restricted {
+        Request storage newRequest = requests[numRequests++];
+        newRequest.description = description;
+        newRequest.value = value;
+        newRequest.vendor = vendor;
+        newRequest.complete = false;
+        newRequest.yesVoterCount = 0;
+    }
 
-}
+    function acceptRequest(uint index) public {
+        Request storage request = requests[index];
+        require(isContributers[msg.sender]);
+        require(!request.yesVoters[msg.sender]);
+        request.yesVoters[msg.sender] = true;
+        request.yesVoterCount++;
+    }
 
-function acceptVote(uint index) public {
-    Request storage request=requests[index];
-    require(isContributers[msg.sender]);
-    require(!request.yesVoters[msg.sender]);
-    request.yesVoters[msg.sender]=true;
-    request.yesVoterCount++;
-}
-
-
-
-
-
-
-
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+        require(request.yesVoterCount > isContributersCount / 2);
+        require(!request.complete);
+        request.vendor.transfer(request.value);
+        request.complete = true;
+    }
 }
